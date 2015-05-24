2729dd8c9ed3f1d97abfaff394e9954a4a983033
markhulia <markhulia@gmail.com>
2015-05-24 23:26:05 +0200

    rowNr
    GetFirstRow is pointed to randomCrap.php and binds values dynamically
    to rowNr

--- app/src/main/java/com/example/mysqltest/getFirstRow.java
+++ app/src/main/java/com/example/mysqltest/getFirstRow.java
@@ -6,11 +6,15 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
 
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by markhulia on 24/05/15.
@@ -18,12 +22,12 @@ import org.json.JSONObject;
 public class GetFirstRow extends Activity {
 
 
-    private static final String GET_FIRST_ROW_URL = URL.URL + "getFirstRow.php";
-    private static final String TAG_ITEMS_REPORT = "items_report";
     public static final String TAG_ITEM_ID = "item_id";
+    private static final String GET_FIRST_ROW_URL = URL.URL + "randomCrap.php";
+    private static final String TAG_ITEMS_REPORT = "items_report";
     String itemIdString;
+    String TAG = "GetFirstRow";
     private JSONArray mList = null;
-
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -43,24 +47,40 @@ public class GetFirstRow extends Activity {
 
         @Override
         protected String doInBackground(String... strings) {
+
+
+            try {
+                List<NameValuePair> params = new ArrayList<NameValuePair>();
+                params.add(new BasicNameValuePair("rowNr", "5"));
+
+                JSONParser jsonParser = new JSONParser();
+                JSONObject jsonObject = jsonParser.makeHttpRequest(
+                        GET_FIRST_ROW_URL, "POST", params);
+
+                Log.d(TAG, jsonObject.toString());
+            } catch (Exception e) {
+                Log.d(TAG, "some exception");
+            }
+
             JSONParser jParser = new JSONParser();
             JSONObject json = jParser.getJSONFromUrl(GET_FIRST_ROW_URL);
 
+
             try {
 
+
                 mList = json.getJSONArray(TAG_ITEMS_REPORT);
-                Log.d(" In mList", " Procees");
-                //0 is first element from the JSON object
-                    JSONObject c = mList.getJSONObject(0);
+                // PAY ATTENTION TO i < 2 ==========================================================
+                JSONObject c = mList.getJSONObject(0);
 
-                    // gets the content of each tag
-                    itemIdString = c.getString(TAG_ITEM_ID);
-                    Log.d(" Item ID ", itemIdString);
-                    //Toast.makeText(GetFirstRow.this, "1st item id " + itemIdString, Toast.LENGTH_LONG).show();
+                // gets the content of each tag
+                itemIdString = c.getString(TAG_ITEM_ID);
+                Log.d(" Item ID ", itemIdString);
+                //Toast.makeText(GetFirstRow.this, "1st item id " + itemIdString, Toast.LENGTH_LONG).show();
 
-                    Intent intent = new Intent(GetFirstRow.this, NotificationBuilder.class);
-                    finish();
-                    startActivity(intent);
+                Intent intent = new Intent(GetFirstRow.this, NotificationBuilder.class);
+                finish();
+                startActivity(intent);
 
             } catch (JSONException e) {
                 e.printStackTrace();
